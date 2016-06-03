df12b0940431a123279fddd19b7716650c1a271e
MKS-students <mks.students@hackreactor.com>
2016-06-03 15:45:42 -0500

    tree complete

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,8 +1,15 @@
+var extend = function(to, from) {
+    for (var key in from) {
+      to[key] = from[key];
+    }
+  };
+
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
+  // newTree.size = 0;
+  extend(newTree,treeMethods )
 
-  // your code here
   newTree.children = null;  // fix me
 
   return newTree;
@@ -15,12 +22,32 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  if(this.children === null) {
+    this.children = [];
+  }
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
 
+
+  if(this.value === target) {
+      return true;
+   }
+
+    var chldrn = this.children;
+
+    if(chldrn !== null) {
+      for(var i = 0; i < chldrn.length; i++) {
+        if(chldrn[i].contains(target)){
+          return true;
+        }
+      }
+    }
+    return false;
 };
+
+
 
 
 /*
