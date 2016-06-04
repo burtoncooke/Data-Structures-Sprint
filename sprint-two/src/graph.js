eebcf5f885de251bcf583fe66b3ecf8e4b368cb3
MKS-students <mks.students@hackreactor.com>
2016-06-04 10:11:31 -0500

    graph and set completed

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,93 @@
 
-
 var Graph = function(){
+ this.nodes = [];
+
 };
 
 Graph.prototype.addNode = function(node){
+
+ this.nodes.push({value: node, edges: []});
+
+
 };
 
 Graph.prototype.contains = function(node){
+
+ for(var i = 0; i < this.nodes.length; i++) {
+   if(this.nodes[i].value === node) {
+     return true;
+   }
+ }
+ return false;
 };
 
 Graph.prototype.removeNode = function(node){
+
+ for(var i = 0; i < this.nodes.length; i++) {
+   if(this.nodes[i].value === node) {
+     this.nodes.splice(i, i + 1);
+   }
+ }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+
+ var has = true;
+
+ for(var i = 0; i < this.nodes.length; i++) {
+   var node = this.nodes[i];
+   if(node.value === fromNode) {
+     if(node.edges.includes(toNode) === false) {
+       has =  false;
+     }
+   }
+   if(node.value === toNode) {
+     if(node.edges.includes(fromNode) === false) {
+       has = false;
+     }
+   }
+ }
+
+ return has;
+
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+
+ for(var i = 0; i < this.nodes.length; i++) {
+
+   var val = this.nodes[i].value;
+   if( val === fromNode) {
+     this.nodes[i].edges.push(toNode);
+   }
+   if(val === toNode) {
+     this.nodes[i].edges.push(fromNode);
+   }
+ }
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+
+ for(var i = 0; i < this.nodes.length; i++) {
+
+   var node = this.nodes[i];
+   if(node.value === fromNode) {
+     var index = node.edges.indexOf(toNode);
+     node.edges.splice(index, index + 1);
+   }
+   if(node.value === toNode) {
+     var index = node.edges.indexOf(fromNode);
+     node.edges.splice(index, index + 1);
+   }
+ }
 };
 
 Graph.prototype.forEachNode = function(cb){
+
+ _.each(this.nodes, function(element) {
+   cb(element.value);
+ })
 };
 
 /*
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,40 @@
 var Set = function(){
-  var set = Object.create(setPrototype);
-  set._storage = undefined;
-  return set;
+ var set = Object.create(setPrototype);
+ set._storage = [];
+ return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+ this._storage.push(item);
 };
 
 setPrototype.contains = function(item){
+ for(var i = 0; i < this._storage.length; i++) {
+   if(this._storage[i] === item) {
+     return true;
+   }
+ }
+ return false;
 };
 
 setPrototype.remove = function(item){
+ // _.each(this._storage, function(element) {
+ //   if(element === item) {
+     // var index = this._storage.indexOf(item)
+     // this._storage.splice(index, index + 1);
+     // break;
+ //   }
+
+ // })
+ for(var i = 0; i < this._storage.length; i++) {
+   if(this._storage[i] === item) {
+     var index = this._storage.indexOf(item);
+     this._storage.splice(index, index + 1);
+     break;
+   }
+ }
 };
 
 /*
